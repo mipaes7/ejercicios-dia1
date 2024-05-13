b189885611688c69bf2eb85594f15558f99883bc
Miguel Pardal <miguelpardalesparis@gmail.com>
2024-05-13 16:08:42 +0200

    solucion con filter en ejercicio1.js

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,10 +1,18 @@
-function countLetters(str, letra) {
-    let count = 0
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === letra) count++;
-    }
+// function countLetters(str, letra) {
+//     let count = 0
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === letra) count++;
+//     }
 
-    return count;
+//     return count;
+// }
+
+function countLetters(str, letra) {
+    let arr = str.split('');
+
+    let result = arr.filter(i => i === letra);
+
+    return result.length;
 }
 
 console.log(countLetters('aaaaaabb', 'a'));
